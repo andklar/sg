6ca4fb9bf20b781d63bee72d03944e48d541e9e2
andklar <andrewklar@hotmail.com>
2016-07-22 14:52:23 -0400

    manually merged's subash's jquery and styles into main

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,32 @@
+$(document).on('ready', function() { 
+ 
+// fade in modal window 
+  // $('.taskname').on('click', function(){ 
+  // $('.modal').fadeIn('slow');
+  // console.log('JIJIJIJIJI'); 
+  // });  
+ 
+});
+
+ 
+// // fade out modal window 
+//   $('button.close').on('click', function(){ 
+//     $('.modal').fadeOut('slow'); 
+//   }); 
+ 
+// // add .errorClass to both input elements 
+//   $('button.submit').on('click', function(e){ 
+//     $('input').addClass('error'); 
+//     e.stopPropagation(); 
+//   }); 
+ 
+// // remove errorClass when user puts cursor in one of input fields 
+//   $('input').on('click', function(e){ 
+//     $(this).removeClass('error'); 
+//     e.stopPropagation(); 
+//   }); 
+ 
+// // Bonus: Allow the user to click the .modal to close itself 
+//   $('.modal').on('click', function(){ 
+//     $(this).fadeOut('slow'); 
+//   });
