a4306681b2617e1c5f93549da0e98573c5823a4e
HankMallard <howardl01@live.ca>
2016-07-25 19:49:49 -0400

    Removed Carrierwave

--- config/initializers/carrierwave.rb
+++
@@ -1,9 +0,0 @@
-require 'carrierwave/orm/activerecord'
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider:                         'Google',
-    google_storage_access_key_id:
-    google_storage_secret_access_key:
-  }
-  config.fog_directory = 'fog_directory'
-end
